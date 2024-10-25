fab605862780f95a1ea5ac8426d11b113d93a2f4
FLG\jordy.lemus <jordy.lemus@forzadelivery.com>
2024-10-24 23:45:37 -0600

    Descripción de los cambios realizados en page.tsx

--- app/(routes)/Proyects/[idProyect]/[idHistoria]/page.tsx
+++ app/(routes)/Proyects/[idProyect]/[idHistoria]/page.tsx
@@ -1,33 +1,36 @@
-import { db } from "@/lib/db"
-import { auth } from "@clerk/nextjs"
-import { redirect } from "next/navigation"
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-import { VerHistoria } from "./VerHistoria"
+import { VerHistoria } from "./VerHistoria";
 
-export default async function HistoriaIdPage({params}: {params: {idHistoria: string}}){
-    //codigo para que al momento de intntar acceder a la pgina sin logear
-    const { userId } = auth()
+export default async function HistoriaIdPage({ params }: { params: { idHistoria: string } }) {
+    const { userId } = auth();
 
-    if(!userId){
-        return redirect("/")
+    if (!userId) {
+        return redirect("/");
     }
 
     const historia = await db.historia.findUnique({
-        where:{
+        where: {
             idHistoria: params.idHistoria,
-           
-        }
-    })
-    if(!historia){
-        return("/")
+        },
+    });
+    
+    if (!historia) {
+        return redirect("/"); 
     }
-  
-    return(
-        <div>
 
-            <VerHistoria historia={historia}/>
-            
+    const setOpen = (open: boolean) => {
+        console.log("Set Open:", open);
+    };
+    
+    const historiaId = historia.idHistoria;
+
+    return (
+        <div>
+            <VerHistoria historia={historia} setOpen={setOpen} historiaId={historiaId} />
             <p>Footer proyect</p>
         </div>
-    )
+    );
 }
